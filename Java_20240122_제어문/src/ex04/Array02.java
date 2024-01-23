1ce7d77a4d5ce590532b36cdd9fef5f60997ca73
daechul <rlaeocjf@gmail.com>
2024-01-23 14:29:59 +0900

    test

--- Java_20240122_제어문/src/ex04/Array02.java
+++ Java_20240122_제어문/src/ex04/Array02.java
@@ -7,8 +7,8 @@ public class Array02 {
 		//1. int형 배열 생성후 랜덤(1~100)이용해서 배열 크기 10 값 저장
 		//2. 그 배열 총점. 평균 구하기
 		
-		double avg;
-		int total;
+		double avg=0;
+		int total=0;
 		
 		int[] arrNum = new int[10];  
 		
@@ -21,6 +21,20 @@ public class Array02 {
 		//배열 10개값 출력
 		for(int i=0; i<arrNum.length; i++)
 			System.out.print( arrNum[i] + " ");
+		
+		System.out.println("\n---------------------------------");
+		
+		// 배열 총점. 평균 구하기
+		for(int i=0; i<arrNum.length; i++) 
+			total += arrNum[i];
+		
+		avg = (double)total/arrNum.length;
+		System.out.println("총점 : " + total + ", 평균 : " + avg);
+		
+		
+		
+		
+		
 		
 	}
 
