3512dc6d718a35f82bdc838b459a342fe2bbe84f
daechul <rlaeocjf@gmail.com>
2024-02-08 14:40:05 +0900

    test

---
+++ Java_20240207_제네릭/src/ex10/GenericMethod01.java
@@ -0,0 +1,63 @@
+package ex10;
+
+class Box<T>{   //T --> String
+	private T obj;
+	
+	void set(T obj) {
+		this.obj = obj;
+	}
+	
+	T get() {
+		return this.obj;
+	}
+}
+
+class BoxFactory{
+	public static <T extends Number> Box<T> makeBox(T o){
+		Box<T> box = new Box();
+		box.set(o);
+		
+		System.out.println("Boxed : "+ o.doubleValue());
+		return box;
+	}
+}
+
+class Unboxer{
+	public static <T extends Number> T openBox(Box<T> box) {
+		System.out.println("unboxed : " + box.get().doubleValue());
+		return box.get();
+	}
+}
+
+
+public class GenericMethod01 {
+
+	public static void main(String[] args) {
+		
+		Box<Double> ibox = BoxFactory.makeBox(10.2);
+		
+		double num = Unboxer.openBox(ibox);
+		System.out.println("data : " + num);
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
