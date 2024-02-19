56e64350b8858a1fd4f8c66f3e6284fb3c05ee17
daechul <rlaeocjf@gmail.com>
2024-02-19 12:02:39 +0900

    test

--- Java_20240216_영화예매/src/movie/AdminMenu.java
+++ Java_20240216_영화예매/src/movie/AdminMenu.java
@@ -29,10 +29,24 @@ public class AdminMenu extends AbstractMenu {
 			return this;
 		case "2" : 
 			printAllMovies();       //영화 목록 출력
-			return this;     //adminMenu(관리자) 반환   
+			return this;     //adminMenu(관리자) 반환  
+		case "3" :
+			deleteMovie();
+			return this;
 		case "b" : return prevMenu; //b입력하면, 이전 메뉴 반환
 		default: return this;  //자기자신(AdminMenu) ==> new AdminMenu(null); 객체
 		}
+	}
+
+	private void deleteMovie() {
+		
+		printAllMovies();  //전체 목록 출력한다.
+		
+		System.out.println("삭제할 영화를 ID값을 입력하세요: ");
+		
+		Movie.delete(sc.nextLine());  
+		System.out.println(">> 삭제되었습니다.");
+		
 	}
 
 	private void createMovie() {
@@ -45,13 +59,9 @@ public class AdminMenu extends AbstractMenu {
 		
 		Movie movie = new Movie(title, genre);
 		
-		try {
-			movie.save();
-			System.out.println(">> 저장되었습니다.");
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-		
+		movie.save();
+		System.out.println(">> 저장되었습니다.");
+	
 	}
 
 	private void printAllMovies() {
--- Java_20240216_영화예매/src/movie/Movie.java
+++ Java_20240216_영화예매/src/movie/Movie.java
@@ -104,9 +104,46 @@ public class Movie {
 	private String toFileString() {
 		return String.format("%d,%s,%s", id,title, genre);
 	}
+                              //1708309377
+	public static void delete(String movieId) {
+		
+		BufferedReader br = null;
+		String text = "";
+		String line = "";
+		
+		try {
+			br = new BufferedReader(new FileReader(file));
+			
+			while((line=br.readLine() )!=null ) {
+				String[] temp = line.split(",");
+				if(movieId.equals(temp[0])) {
+					continue;
+				}
+				text += line + "\n";
+			}
+			br.close();
+			
+			FileWriter fw = new FileWriter(file);
+			fw.write(text);
+			
+			fw.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 	
 	
 }
+
+
+
+
+
+
+
+
+
+
 
 
 
